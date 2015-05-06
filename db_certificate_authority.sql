2039f6be3210d76c3c5dcb9428ac4c4e9de34c19
alvarisi <alvarisi01@gmail.com>
2015-05-06 15:28:44 +0700

    sql

--- db_certificate_authority.sql
+++ db_certificate_authority.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2015 at 03:02 AM
+-- Generation Time: May 06, 2015 at 10:02 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.5.19
 
@@ -38,17 +38,37 @@ CREATE TABLE IF NOT EXISTS `certificate` (
   `user_id` int(11) NOT NULL,
   `status` char(1) NOT NULL,
   `created_at` datetime NOT NULL,
-  `valid_at` datetime NOT NULL,
-  `sign_stat` int(11) DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `valid_at` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `certificate`
 --
 
-INSERT INTO `certificate` (`id`, `country_name`, `state_name`, `locality_name`, `organization_name`, `organizational_unit`, `common_name`, `email_address`, `user_id`, `status`, `created_at`, `valid_at`, `sign_stat`) VALUES
-(1, 'Indonesia', 'Jawa Timur', 'Asia/Jakarta', 'ITS', 'TC', 'Fatahillah', 'alvarisi@live.com', 0, '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0),
-(3, 'Indonesia', 'Bali', 'Denpasar', 'ITS', 'TC', 'Andre Napitupulu', 'yosuaandre@gmail.com', 1, '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0);
+INSERT INTO `certificate` (`id`, `country_name`, `state_name`, `locality_name`, `organization_name`, `organizational_unit`, `common_name`, `email_address`, `user_id`, `status`, `created_at`, `valid_at`) VALUES
+(1, 'Indonesia', 'Jawa Timur', 'Asia/Jakarta', 'ITS', 'TC', 'Fatahillah', 'alvarisi@live.com', 0, '', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'ID', 'Jawa Timur', 'Surabaya', 'ITS', 'tec', 'yes', 'alvarisi@live.com', 1, '', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country`
+--
+
+CREATE TABLE IF NOT EXISTS `country` (
+  `code` char(4) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `country`
+--
+
+INSERT INTO `country` (`code`, `name`) VALUES
+('CA', 'Canada'),
+('ID', 'Indonesia'),
+('JP', 'Japan'),
+('US', 'United States of America');
 
 -- --------------------------------------------------------
 
@@ -58,19 +78,17 @@ INSERT INTO `certificate` (`id`, `country_name`, `state_name`, `locality_name`, 
 
 CREATE TABLE IF NOT EXISTS `user` (
 `id` int(11) NOT NULL,
-  `nama` varchar(100) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `level` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `nama`, `email`, `password`, `level`) VALUES
-(1, 'Andre Napitupulu', 'yosua.andre@gmail.com', 'andrejoshua', 2),
-(2, 'administrator', 'admin@kij.com', 'admin', 1);
+INSERT INTO `user` (`id`, `email`, `password`, `level`) VALUES
+(1, 'a@a.com', '12345', 2);
 
 --
 -- Indexes for dumped tables
@@ -81,6 +99,12 @@ INSERT INTO `user` (`id`, `nama`, `email`, `password`, `level`) VALUES
 --
 ALTER TABLE `certificate`
  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `country`
+--
+ALTER TABLE `country`
+ ADD PRIMARY KEY (`code`);
 
 --
 -- Indexes for table `user`
@@ -96,12 +120,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `certificate`
 --
 ALTER TABLE `certificate`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
